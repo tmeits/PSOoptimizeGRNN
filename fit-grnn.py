b9fe0d19b29a50803b90e33f241788a6e33cf66a
tmeits <tmeits@gmail.com>
2017-04-26 16:42:02 +0700

    Add files via upload

---
+++ fit-grnn.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 26 12:21:48 2017
+
+@author: IVA
+"""
+#%%
+# https://statcompute.wordpress.com/2015/12/09/fitting-generalized-regression-neural-network-with-python/
+# http://www.lwebzem.com/cgi-bin/res/view.cgi?name=gr_nn1.cgi
+# http://freeconnection.blogspot.ru/2012/10/general-regression-neural-network-grnn.html
+#%%
+#%%
+#%%
